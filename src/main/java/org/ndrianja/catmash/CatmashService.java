328ffd1bf6f5a1ad3ce12a63972e66d5d7baa6e2
Ndrianja Rakotomanga <ndrianjadr@gmail.com>
2019-09-30 15:30:56 +0200

    #1 Better cat selection - Fixing code coverage issue

--- src/main/java/org/ndrianja/catmash/CatmashService.java
+++ src/main/java/org/ndrianja/catmash/CatmashService.java
@@ -15,30 +15,17 @@ public class CatmashService {
 
     public CatmashRepository getCats() {
         if (catmashRepository == null) {
-            /*
-             * Client client = ClientBuilder.newClient(); WebTarget webTarget =
-             * client.target("https://latelier.co/data/cats.json"); Invocation.Builder invocationBuilder =
-             * webTarget.request(MediaType.APPLICATION_JSON); Response response = invocationBuilder.get();
-             */
             ObjectMapper objectMapper = new ObjectMapper();
             try {
                 InputStream file = getClass().getClassLoader().getResourceAsStream("cats.json");
                 return catmashRepository = objectMapper.readValue(file, CatmashRepository.class);
-            } catch (JsonParseException e) {
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
         } else {
             return catmashRepository;
         }
-    }
-
-    public void setCats(CatmashRepository cats) {
-        this.catmashRepository = cats;
     }
 
     public List<CatImage> getImages() {
